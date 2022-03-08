05d9fa52461ac089ca8a53d298781179f493ad72
Andrej Sharapov <an.sharapov90@ya.ru>
2022-03-08 23:56:02 +0300

    added breakpoints

--- src/components/css-line-clamp.vue
+++ src/components/css-line-clamp.vue
@@ -1,14 +1,27 @@
 <template>
+<div class="d-grid gap-6">
   <div class="d-grid grid-cols-sm-3 gap-6">
     <div v-for="(item, index) in clamps" :key="index" flat tile class="border">
-      <v-card-subtitle>
+      <v-subheader>
         {{ `line-clamp-${item}` }}
-      </v-card-subtitle>
+      </v-subheader>
       <v-card-text>
         <p class="mb-0" :class="`line-clamp-${item}`">{{ msg }}</p>
       </v-card-text>
     </div>
   </div>
+
+  <div class="d-grid grid-cols-sm-3 gap-6">
+    <div v-for="(item, index) in clamps" :key="index" flat tile class="border">
+      <v-subheader>
+        {{ `line-clamp-sm-${item} line-clamp-lg-1` }}
+      </v-subheader>
+      <v-card-text>
+        <p class="mb-0" :class="`line-clamp-sm-${item} line-clamp-lg-1`">{{ msg }}</p>
+      </v-card-text>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
