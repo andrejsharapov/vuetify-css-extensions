7a95fcd1b94ff1c86b1ee72af761122c43c80f22
Andrej Sharapov <an.sharapov90@ya.ru>
2022-03-08 23:24:56 +0300

    added breakpoints

--- src/components/css-opacity.vue
+++ src/components/css-opacity.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="d-grid grid-cols-md-6 gap-6">
-    <div v-for="(item, index) in opacity" :key="index">
-      <div class="pa-4 text-center border" :class="`opacity-${item}`">
-        <p class="mb-0">{{ `opacity-${item}` }}</p>
+  <div class="d-grid gap-6">
+    <v-subheader>opacity-*</v-subheader>
+    <div class="d-grid grid-cols-md-6 gap-6">
+      <div v-for="(item, index) in opacity" :key="index">
+        <div class="pa-4 text-center border" :class="`opacity-${item}`">
+          <p class="mb-0">{{ `opacity-${item}` }}</p>
+        </div>
+      </div>
+    </div>
+
+    <v-subheader>opacity-lg-*</v-subheader>
+    <div class="d-grid grid-cols-md-6 gap-6">
+      <div v-for="(item, index) in opacity" :key="index">
+        <div class="pa-4 text-center border" :class="`opacity-lg-${item}`">
+          <p class="mb-0">{{ `opacity-lg-${item}` }}</p>
+        </div>
       </div>
     </div>
   </div>
